18a80b97c2cdb0f8ae58b23d178ed9a36b3973a9
primedevintern10 <primedevintern10@gmail.com>
2025-10-16 19:18:16 +0530

    fixed the alignment issue2

--- app/try/Questionaire.js
+++ app/try/Questionaire.js
@@ -1760,7 +1760,7 @@ export default function Questionnaire() {
         </div>
       </div>
 
-      {/* Content with proper spacing */}
+
       <div className="flex-grow flex flex-col items-center justify-center py-10 px-4 ">
         <div className="w-full max-w-lg mb-10 text-center">
           <h2 className="text-2xl md:text-3xl font-bold mb-4 text-gray-900">
